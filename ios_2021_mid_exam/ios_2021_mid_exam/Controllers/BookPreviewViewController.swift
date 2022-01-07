2f0653bec74d4ec5744217dc3095c42d32d671f1
Ivan Stoilov <idstoilov@gmail.com>
2022-01-07 19:25:44 +0200

    Done with book preview view

---
+++ ios_2021_mid_exam/ios_2021_mid_exam/Controllers/BookPreviewViewController.swift
@@ -0,0 +1,14 @@
+//
+//  BookPreviewViewController.swift
+//  ios_2021_mid_exam
+//
+//  Created by Ivan Stoilov on 7.01.22.
+//
+
+import UIKit
+
+final class BookPreviewViewController: UIViewController {
+    
+    
+    
+}
