d0515702cb3502f35f599741e4076127f79fb88c
Ivan Stoilov <idstoilov@gmail.com>
2022-01-07 18:45:13 +0200

    Added some UI elements
    added table view and xib for it

---
+++ ios_2021_mid_exam/ios_2021_mid_exam/CustomUI/BookTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BookTableViewCell.swift
+//  ios_2021_mid_exam
+//
+//  Created by Ivan Stoilov on 7.01.22.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
