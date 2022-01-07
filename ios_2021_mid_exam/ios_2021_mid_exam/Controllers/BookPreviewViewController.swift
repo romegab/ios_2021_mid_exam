4f24e14e534db41a386e0ff7c36dd054ac654afa
Ivan Stoilov <idstoilov@gmail.com>
2022-01-07 19:34:51 +0200

    Added book preview controller

--- ios_2021_mid_exam/ios_2021_mid_exam/Controllers/BookPreviewViewController.swift
+++ ios_2021_mid_exam/ios_2021_mid_exam/Controllers/BookPreviewViewController.swift
@@ -1,5 +1,5 @@
 //
-//  BookPreviewViewController.swift
+//  ViewController.swift
 //  ios_2021_mid_exam
 //
 //  Created by Ivan Stoilov on 7.01.22.
@@ -8,7 +8,31 @@
 import UIKit
 
 final class BookPreviewViewController: UIViewController {
+
+    @IBOutlet private weak var bookTitleLabel: UILabel!
+    @IBOutlet private weak var bookImageView: UIImageView!
+    @IBOutlet private weak var bookAuthorLabel: UILabel!
+    @IBOutlet private weak var bookRating: UILabel!
+    @IBOutlet private weak var bookDescription: UITextView!
     
+    public var book: Book? {
+        didSet {
+            updateUI()
+        }
+    }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    private func updateUI() {
+        bookTitleLabel.text = book?.title
+        bookImageView.image = UIImage(named: book?.photo ?? "")
+        bookAuthorLabel.text = book?.author
+        bookRating.text = String(Double(book?.rating ?? 0))
+        bookDescription.text = book?.description
+    }
     
 }
+
