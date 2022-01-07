3a3f2d1fbe6ba9409c9afaaf61f2a8972162f30d
Ivan Stoilov <ivstoilov@melontech.com>
2022-01-07 17:02:59 +0200

    project init

---
+++ ios_2021_mid_exam/ios_2021_mid_exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_2021_mid_exam
+//
+//  Created by Ivan Stoilov on 7.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
