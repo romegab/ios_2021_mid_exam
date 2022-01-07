6c15381636f4d151410fb33c58432a369b6f9a53
Ivan Stoilov <idstoilov@gmail.com>
2022-01-07 18:56:46 +0200

    Finished xib logic

--- ios_2021_mid_exam/ios_2021_mid_exam/CustomUI/BookTableViewCell.swift
+++ ios_2021_mid_exam/ios_2021_mid_exam/CustomUI/BookTableViewCell.swift
@@ -7,16 +7,43 @@
 
 import UIKit
 
-class BookTableViewCell: UITableViewCell {
-
+final class BookTableViewCell: UITableViewCell {
+    
+    @IBOutlet private weak var bookTitleLabel: UILabel!
+    @IBOutlet private weak var bookImageView: UIImageView!
+    @IBOutlet weak var viewButton: UIButton!
+    
+    private var book: Book? {
+        didSet {
+            updateUI()
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setUpCornerRadius()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
+    }
+    
+    private func setUpCornerRadius() {
+        bookImageView.clipsToBounds = true
+        viewButton.clipsToBounds = true
+        
+        bookImageView.layer.cornerRadius = 6
+        viewButton.layer.cornerRadius = 6
+    }
+    
+    private func updateUI() {
+        bookTitleLabel.text = book?.title ?? "title"
+        bookImageView.image = UIImage(named: book?.photo ?? "")
+    }
+    
+    @IBAction private func viewButtonPressed(_ sender: UIButton) {
+        print("view button was pressed")
     }
     
 }
--- ios_2021_mid_exam/ios_2021_mid_exam/ViewController.swift
+++ ios_2021_mid_exam/ios_2021_mid_exam/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private var books = Model().books
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
     }
+    
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    }
+    
 }
 
